23ff45f4a4416495d42bdef2a9fb90d471be0fb5
littleBuddhaa <somineegupta@gmail.com>
2019-04-09 02:20:37 +0530

    sharing but no image show

--- DocumentOrganizer/app/src/main/java/com/bellatrix/aditi/documentorganizer/ImageDetailsActivity.java
+++ DocumentOrganizer/app/src/main/java/com/bellatrix/aditi/documentorganizer/ImageDetailsActivity.java
@@ -1,8 +1,10 @@
 package com.bellatrix.aditi.documentorganizer;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
@@ -16,8 +18,6 @@ import android.widget.TextView;
 import com.bellatrix.aditi.documentorganizer.Database.Contract;
 import com.bellatrix.aditi.documentorganizer.Database.DBQueries;
 
-import static android.provider.MediaStore.Audio.Playlists.Members._ID;
-
 public class ImageDetailsActivity extends AppCompatActivity {
     LinearLayout linearLayout;
     private static String folderName;
@@ -25,6 +25,9 @@ public class ImageDetailsActivity extends AppCompatActivity {
     private int index;
     // Keep reference to the ShareActionProvider from the menu
     private ShareActionProvider shareActionProvider;
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
+
 
 
     @Override
@@ -41,14 +44,34 @@ public class ImageDetailsActivity extends AppCompatActivity {
         mCursor = DBQueries.getImageByFolder(ImageDetailsActivity.this, folderName);
 
         mCursor.moveToPosition(index);
-        ImageView imgView = (ImageView) findViewById(R.id.iv);
+         ImageView imgView = (ImageView) findViewById(R.id.iv);
 
         imgView.setImageURI(Uri.parse((mCursor.getString(mCursor.getColumnIndex(Contract.Documents.COLUMN_URI)))));
+        String url = mCursor.getString(mCursor.getColumnIndex(Contract.Documents.COLUMN_TITLE));
+
+        //sharingg
+       // MenuItem item = findViewById(R.id.menu_share);
+
+
+
 
 
 
     }
 
+    private void shareImage() {
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // If you want to share a png image only, you can do:
+        // setType("image/png"); OR for jpeg: setType("image/jpeg");
+        share.setType("image/BMP");
+
+
+        Uri uri = Uri.parse((mCursor.getString(mCursor.getColumnIndex(Contract.Documents.COLUMN_URI))));
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        startActivity(Intent.createChooser(share, "Share Image!"));
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -56,9 +79,9 @@ public class ImageDetailsActivity extends AppCompatActivity {
         {
             case R.id.action_details:
                  linearLayout = (LinearLayout) findViewById(R.id.mainLayout);
-                linearLayout.setOrientation(LinearLayout.VERTICAL);
+               // linearLayout.setOrientation(LinearLayout.VERTICAL);
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(ImageDetailsActivity.this);
-                long id = mCursor.getLong(mCursor.getColumnIndex(_ID));
+                long id = mCursor.getLong(mCursor.getColumnIndex("_ID"));
                 cursor =  DBQueries.getImageById(ImageDetailsActivity.this, folderName,id);
 
                 View mview = getLayoutInflater().inflate(R.layout.image_details_dialog,null);
@@ -138,6 +161,20 @@ public class ImageDetailsActivity extends AppCompatActivity {
                 AlertDialog dialog = mBuilder.create();
                 dialog.show();
                 return true;
+            case R.id.menu_share:
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                View.OnClickListener handler = new View.OnClickListener() {
+                    public void onClick(View v) {
+                            shareImage();
+                        }
+
+                };
+
+                findViewById(R.id.menu_share).setOnClickListener(handler);
+                return true;
+
+
         }
         return super.onOptionsItemSelected(item);
     }
@@ -145,6 +182,7 @@ public class ImageDetailsActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate menu resource file.
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
         return true;
     }
 
