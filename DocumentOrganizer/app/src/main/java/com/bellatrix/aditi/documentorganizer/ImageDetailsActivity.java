c98f88fadfe4c75f3ce593c27890fb97584d67cd
littleBuddhaa <somineegupta@gmail.com>
2019-04-12 17:26:19 +0530

    changes

--- DocumentOrganizer/app/src/main/java/com/bellatrix/aditi/documentorganizer/ImageDetailsActivity.java
+++ DocumentOrganizer/app/src/main/java/com/bellatrix/aditi/documentorganizer/ImageDetailsActivity.java
@@ -68,43 +68,7 @@ public class ImageDetailsActivity extends AppCompatActivity {
 
 
     }
-    private void shareMyImage(){
 
-        byte[] byteArray = mCursor.getBlob(mCursor.getColumnIndex(Contract.Documents.COLUMN_IMAGE));
-
-        Bitmap bm = BitmapFactory.decodeByteArray(byteArray, 0 ,byteArray.length);
-
-        String name = mCursor.getString(mCursor.getColumnIndex(Contract.Documents.COLUMN_TITLE));
-
-        try {
-
-            File cachePath = new File(getCacheDir(), "images");
-            cachePath.mkdirs(); // don't forget to make the directory
-            //FileOutputStream stream = new FileOutputStream(cachePath + "/" + name  + ".png"); // overwrites this image every time
-            FileOutputStream stream = new FileOutputStream(cachePath + "/image.png");
-            bm.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            stream.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        File imagePath = new File(getCacheDir(), "images");
-        File newFile = new File(imagePath, "image.png");
-        Uri contentUri = FileProvider.getUriForFile(ImageDetailsActivity.this, "com.bellatrix.aditi.fileprovider", newFile);
-
-        if (contentUri != null) {
-            Intent shareIntent = new Intent();
-            shareIntent.setAction(Intent.ACTION_SEND);
-            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); // temp permission for receiving app to read this file
-            shareIntent.setDataAndType(contentUri, getContentResolver().getType(contentUri));
-            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
-            startActivity(Intent.createChooser(shareIntent, "Choose an app"));
-        }
-
-
-
-    }
     private void shareMyImage(){
 
         byte[] byteArray = mCursor.getBlob(mCursor.getColumnIndex(Contract.Documents.COLUMN_IMAGE));
